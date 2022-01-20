216a6e02d551b281f2fa2f2d87457f4120947c34
Baylin Lavy <baylinlavy@ucsb.edu>
2022-01-19 19:51:53 -0800

    submission 1

--- car.cpp
+++ car.cpp
@@ -4,4 +4,83 @@
 #include "car.hpp"
 #include <cstddef>
 #include <cstring>
-// #include .......
+// #include ......
+Car::Car() : manufacturer(NULL), model(NULL), zeroToSixtyNs(0), headonDragCoeff(0), horsepower(0), backseatDoors(None), seatCount(0) {
+
+}
+
+Car::Car(const char* const manufacturerName, const char* const modelName, PerformanceStats perf, uint8_t numSeats, DoorKind backseatDoorDesign): zeroToSixtyNs(perf.zeroToSixtyNs), horsepower(perf.horsepower), headonDragCoeff(perf.headonDragCoeff), backseatDoors(backseatDoorDesign), seatCount(numSeats){
+	manufacturer = (char*)manufacturerName;
+	model = (char*)modelName;
+}
+
+Car::Car(Car const& o){
+	this->manufacturer = (char*)o.getManufacturer();
+    this->model = (char*)o.getModel();
+    this->zeroToSixtyNs = o.zeroToSixtyNs;
+    this->headonDragCoeff = o.headonDragCoeff;
+    this->horsepower = o.horsepower;
+    this->backseatDoors = o.getBackseatDoors();
+    this->seatCount = o.getSeatCount();
+}
+
+Car::~Car(){
+
+}
+
+Car& Car::operator=(Car const& o){
+	if(this == &o){
+		 return *this;
+	 }
+	this->manufacturer = (char*)o.getManufacturer();
+    this->model = (char*)o.getModel();
+    this->zeroToSixtyNs = o.zeroToSixtyNs;
+    this->headonDragCoeff = o.headonDragCoeff;
+    this->horsepower = o.horsepower;
+    this->backseatDoors = o.getBackseatDoors();
+    this->seatCount = o.getSeatCount();
+	return *this;
+}
+
+
+char const* Car::getManufacturer() const{
+	return manufacturer;
+}
+
+char const* Car::getModel() const{
+	return model;
+}
+
+PerformanceStats Car::getStats() const{
+	return PerformanceStats(horsepower, zeroToSixtyNs, headonDragCoeff);
+}
+
+uint8_t Car::getSeatCount() const{
+	return seatCount;
+}
+
+DoorKind Car::getBackseatDoors() const{
+	return backseatDoors;
+}
+
+void Car::manufacturerChange(char const* const newManufacturer){
+	manufacturer = (char*)newManufacturer;
+}
+
+void Car::modelNameChange(char const* const newModelName) {
+	model = (char*)newModelName;
+}
+
+void Car::reevaluateStats(PerformanceStats newStats) {
+	horsepower = newStats.horsepower;
+	zeroToSixtyNs = newStats.zeroToSixtyNs;
+	headonDragCoeff = newStats.headonDragCoeff;
+}
+
+void Car::recountSeats(uint8_t newSeatCount) {
+	seatCount = newSeatCount;
+}
+
+void Car::reexamineDoors(DoorKind newDoorKind) {
+	backseatDoors = newDoorKind;
+}
